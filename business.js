985cf10b6d4c4078d50af92fb9e64779fa0d5086
Hudson Vurbeff <Hudsonvurbeff@gmail.com>
2016-08-11 14:48:01 +0200

    Update business.js

--- business.js
+++ business.js
@@ -1,7 +1,11 @@
 window.onload = function(){
     document.getElementById("zIn").style.animationPlayState = "running";
-    $("#Grad").click(function() {
-  console.log("clicker");
-        $("#ContentHolder").scrollintoview({duration: 500});
+$("#Grad").click(function() {
+    console.log("clicker");
+    $("#ContentHolder").scrollintoview({duration: 500});
+});
+    
+$("#ContactButton").click(function() {
+    window.location.href = "businessContact.html"
 });
 }
